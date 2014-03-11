b1823102acc95d955c25d0150d77247df2190802
Iain Beeston <iain.beeston@gmail.com>
2014-03-11 21:22:23 +0000

    Added proper tests to tasks_spec.rb

--- spec/lib/vendorise/tasks_spec.rb
+++ spec/lib/vendorise/tasks_spec.rb
@@ -12,5 +12,15 @@ module Vendorise
     it "accepts a url argument" do
       expect(task.arg_names).to match_array [:url]
     end
+
+    it "creates a new arborist and passes it's command to system" do
+      fake_arborist = double("Arborist", subtree_command: "touch foo.tmp")
+      allow(Vendorise::Arborist).to receive(:new).with("vendor/gems/vendorise", "git@github.com:New-Bamboo/vendorise.git").and_return(fake_arborist)
+
+      task.invoke("git@github.com:New-Bamboo/vendorise.git")
+
+      expect(File.exist?("foo.tmp")).to be true
+      File.delete("foo.tmp")
+    end
   end
 end
