356325b05a2a13492832a23b1fed3ce75777f191
Iain Beeston <iain.beeston@gmail.com>
2014-03-22 13:01:31 +0000

    Added an optional parameter to the rake task to specify the branch

--- spec/lib/vendorise_spec.rb
+++ spec/lib/vendorise_spec.rb
@@ -2,8 +2,18 @@ require_relative "../spec_helper"
 require_relative "../../lib/vendorise"
 
 describe "vendorise" do
+  after(:each) do
+    # remove expect_any_instance_of stubs
+    RSpec::Mocks.space.reset_all
+  end
+
   it "adds a rake task to run git subtree" do
     expect_any_instance_of(Rake::FileUtilsExt).to receive(:sh).with("git subtree add --prefix vendor/gems/sinatra git@github.com:sinatra/sinatra.git master --squash")
-    Rake::Task["vendorise:gem"].invoke("git@github.com:sinatra/sinatra.git")
+    Rake.application.invoke_task "vendorise:gem[git@github.com:sinatra/sinatra.git]"
+  end
+
+  it "uses the (optional) second argument to select the branch" do
+    expect_any_instance_of(Rake::FileUtilsExt).to receive(:sh).with("git subtree add --prefix vendor/gems/sinatra git@github.com:sinatra/sinatra.git 1.3.x --squash")
+    Rake.application.invoke_task "vendorise:gem[git@github.com:sinatra/sinatra.git, 1.3.x]"
   end
 end
