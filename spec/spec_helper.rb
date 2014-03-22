44b831a180c73a8aefcfe22da8020ec8254afd69
Iain Beeston <iain.beeston@gmail.com>
2014-03-22 13:31:47 +0000

    Stopped running tests that use expect_any_instance on jruby
    It doesn't seem to work on jruby-1.7.9-d19 and
    rspec-mocks-2.14.6 (gem functionality should be fine though)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@ RSpec.configure do |config|
   config.expect_with(:rspec){ |c| c.syntax = :expect }
   config.mock_with(:rspec){ |c| c.syntax = :expect }
 
+  # don't complain that I'm using symbols as metadata...
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  # reseting the mock space doesn't seem to work on jruby
+  config.filter_run_excluding :clear_mocks if RUBY_ENGINE == "jruby"
   config.after(:each, :clear_mocks) do
     # expect_any_instance_of hangs around between specs
     RSpec::Mocks.space.reset_all
