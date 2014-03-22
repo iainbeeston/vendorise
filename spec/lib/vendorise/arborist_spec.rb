354530aa0870d61a86960e814c6239489e296797
Iain Beeston <iain.beeston@gmail.com>
2014-03-22 12:10:43 +0000

    Refactored arborist spec out so that it's easier to extend

--- spec/lib/vendorise/arborist_spec.rb
+++ spec/lib/vendorise/arborist_spec.rb
@@ -3,21 +3,21 @@ require_relative "../../../lib/vendorise/arborist"
 
 module Vendorise
   describe Arborist do
-    describe :subtree_command do
+    describe :subtree_already_exists? do
       subject { Arborist.new "foo/bar", "git@github.com:New-Bamboo/vendorise.git" }
 
-      context "when #subtree_already_exists? is false" do
+      context "is false" do
         before { allow(subject).to receive(:subtree_already_exists?).and_return(false) }
 
-        it "is git subtree add" do
+        it "makes #subtree_command use 'git subtree add'" do
           expect(subject.subtree_command).to eq "git subtree add --prefix foo/bar git@github.com:New-Bamboo/vendorise.git master --squash"
         end
       end
 
-      context "when #subtree_already_exists? is true" do
+      context "is true" do
         before { allow(subject).to receive(:subtree_already_exists?).and_return(true) }
 
-        it "is git subtree pull" do
+        it "makes #subtree_command use 'git subtree pull'" do
           expect(subject.subtree_command).to eq "git subtree pull --prefix foo/bar git@github.com:New-Bamboo/vendorise.git master --squash"
         end
       end
