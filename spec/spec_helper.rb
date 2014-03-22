6531c67fd48f8d7937722d18ad330d44e34f5e9a
Iain Beeston <iain.beeston@gmail.com>
2014-03-22 13:19:49 +0000

    Refactored code to clear mocks out to a hook

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,9 @@ RSpec.configure do |config|
   config.order = 'random'
   config.expect_with(:rspec){ |c| c.syntax = :expect }
   config.mock_with(:rspec){ |c| c.syntax = :expect }
+
+  config.after(:each, :clear_mocks) do
+    # expect_any_instance_of hangs around between specs
+    RSpec::Mocks.space.reset_all
+  end
 end
