a0da3165d8053220df736514e340d6eadcb7eb65
Iain Beeston <iain.beeston@gmail.com>
2014-03-23 12:46:55 +0000

    Fixed up vendorise specs
    1. They weren't actually running the rake task each time due to the fact that
    invoke does not re-run tasks if repeatedly called
    2. Fixed up stubbing to be lower level and not require the use of any_instance

--- spec/lib/vendorise_spec.rb
+++ spec/lib/vendorise_spec.rb
@@ -2,13 +2,21 @@ require_relative "../spec_helper"
 require_relative "../../lib/vendorise"
 
 describe "vendorise" do
-  it "adds a rake task to run git subtree", :clear_mocks do
-    expect_any_instance_of(Rake::FileUtilsExt).to receive(:sh).with("git subtree add --prefix vendor/gems/sinatra git@github.com:sinatra/sinatra.git master --squash")
+  before do
+    Rake::Task["vendorise:gem"].reenable
+  end
+
+  it "adds a rake task to run git subtree" do
+    expect(Rake::AltSystem).to receive(:system) do |cmd|
+      expect(cmd).to eq("git subtree add --prefix vendor/gems/sinatra git@github.com:sinatra/sinatra.git master --squash")
+    end
     Rake.application.invoke_task "vendorise:gem[git@github.com:sinatra/sinatra.git]"
   end
 
-  it "uses the (optional) second argument to select the branch", :clear_mocks do
-    expect_any_instance_of(Rake::FileUtilsExt).to receive(:sh).with("git subtree add --prefix vendor/gems/sinatra git@github.com:sinatra/sinatra.git 1.3.x --squash")
+  it "uses the (optional) second argument to select the branch" do
+    expect(Rake::AltSystem).to receive(:system) do |cmd|
+      expect(cmd).to eq("git subtree add --prefix vendor/gems/sinatra git@github.com:sinatra/sinatra.git 1.3.x --squash")
+    end
     Rake.application.invoke_task "vendorise:gem[git@github.com:sinatra/sinatra.git, 1.3.x]"
   end
 end
